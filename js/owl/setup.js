5c6fd3400e72551b71908a44693c41d9bc755e2f
Felipe <91050670+FelipeFama@users.noreply.github.com>
2022-02-06 01:59:15 -0300

    Arquivos do projeto

---
+++ js/owl/setup.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 5
+        }
+    }
+})
+
+$(document).ready(function () {
+    $('.menu-toggle').click(function () {
+        $('nav').toggleClass('active')
+    })
+})
